11620d554935c92aefc406e213c06fc73bca0623
psegarra <pausegarra.b@gmail.com>
2024-02-03 21:02:08 +0100

    🍺 ad ukrainian

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
 export enum Languages {
   spanish_es = 'Spanish',
   english_uk = 'English (UK)',
-  russian_ru = 'Russian'
+  russian_ru = 'Russian',
+  ukrainian = 'Ukrainian'
 }
 
 export const OPENAI_KEY = process.env.OPENAI_API_KEY as string;
